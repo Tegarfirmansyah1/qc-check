5ec2013caa7ad7d8fd4c60f1a32b7895a8951723
gars122 <garstough@gmail.com>
2025-07-10 09:51:21 +0700

    Finalize all features and prepare for deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    // 👇 TAMBAHKAN BLOK INI
+    server: {
+        host: '0.0.0.0', // Memungkinkan Vite diakses dari jaringan
+        hmr: {
+            host: 'localhost',
+        },
+    },
+    // END TAMBAHAN
+
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
